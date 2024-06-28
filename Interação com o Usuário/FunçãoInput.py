ed6e955ebfa7eb75f6ad893cbe196cf16fb53ab1
Rhaissa Rodrigues <106785478+RhaissaRR@users.noreply.github.com>
2024-06-28 13:42:37 -0300

    Add files via upload

---
+++ Interação com o Usuário/FunçãoInput.py
@@ -0,0 +1,11 @@
+print("Conta-me qualquer coisa...")
+anything = input()
+print("Hum...", anything, "... Realmente?")
+
+#A função input solicita que o usuario insira alguns dados 
+
+#função INPUT com argumento 
+anything = input("Conta-me qualquer coisa...")
+print("Hum...", anything, "...Realmente?")
+#a função input é chamada com um argumento(uma string contendo um argumento)
+#
---
+++ Interação com o Usuário/Operações Proibidas.py
@@ -0,0 +1,5 @@
+#Aqui vai dar uma operação proibida ou seja uma mensagem de TypeError
+anything = input("Digite um número: ")
+something = anything ** 2.0
+print(anything, "elevado a 2 é", something)
+
